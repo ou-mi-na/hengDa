8908b0238a666d6aa4d2c77b8d1d8c3907d01113
omina <omina@itast.cn>
2020-10-05 16:17:44 +0800

    master

---
+++ item.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+  var x = document.getElementById("passsword").value;
+  var y = document.getElementById("tijiao");
+  if ((x = 123456)) {
+    function fun() {
+      location.assign("https://www.baidu.com/");
+    }
+  } else {
+  }
+};
